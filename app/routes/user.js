bd84b80976fac6c90cfa87ffb1525e030164809b
djvoa12 <djvoa12@gmail.com>
2015-03-03 16:14:48 -0800

    added initial user.js route promises

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,8 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
-    var user = Ember.$.getJSON('https://api.github.com/users/' + params.username).then(function(data) {
-    	debugger
-		});
-	}
+    var promise1 = new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax({
+        type: "GET",
+        url: 'https://api.github.com/users/' + params.username,
+        success: resolve,
+        error: reject
+      });
+    }).then(function(response) {
+      return response;
+    }, function() {});
+
+    var promise2 = new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax({
+        type: "GET",
+        url: 'https://api.github.com/users/' + params.username + "/repos",
+        success: resolve,
+        error: reject
+      });
+    }).then(function(response) {
+      return response;
+    }, function() {});
+
+    return Ember.RSVP.hash({
+      user: promise1,
+      repos: promise2
+    });
+  }
 });
